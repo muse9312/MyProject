2680204927d934a2d44d56599ffd8b17a8f30ee4
muse9312 <muse9312@naver.com>
2021-12-03 19:42:55 +0900

    FullStack : Create User & UserController & UesrRepository

---
+++ Exercise/Spring Boot + React/FullStack/backend/src/main/java/io/mehesh/backend/User.java
@@ -0,0 +1,21 @@
+package io.mehesh.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String Name;
+    private String Email;
+    private String Password;
+
+}
