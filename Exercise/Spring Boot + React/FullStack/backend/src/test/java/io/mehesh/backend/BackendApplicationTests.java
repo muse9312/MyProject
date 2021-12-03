78bf2f778c2de5f165faa96b3900b7230575bf98
muse9312 <muse9312@naver.com>
2021-12-03 18:45:55 +0900

    Create : FullStack(Spring + React + MySql)

---
+++ Exercise/Spring Boot + React/FullStack/backend/src/test/java/io/mehesh/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mehesh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Exercise/Spring Boot + React/FullStack/frontend/src/App.js
+++ Exercise/Spring Boot + React/FullStack/frontend/src/App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>  
+      </header>
     </div>
   );
 }
